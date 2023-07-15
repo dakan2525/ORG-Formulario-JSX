cbbdf167b6047fc33fe841b199afd5aa4fa58c23
Dakan <andres25prado@gmail.com>
2023-07-14 19:50:32 -0500

    actualizacion de iconos y estilos css

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -2,19 +2,19 @@ import "./Footer.css"
 
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)"}}>
+    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.webp)"}}>
         <div className='redes'>
             <a href='https://www.linkedin.com/in/andres-felipe-prado-velasco/'>
-                <img src="/img/Linkedin.png" alt='Linkedin' />
+                <img src="/img/linkedinLogo.svg" alt='Linkedin' />
             </a>
             <a href='https://github.com/dakan2525'>
-                <img src="/img/R.png" alt='Gihub' />
+                <img src="/img/githubLogo.svg" alt='Gihub' />
             </a>
             <a href='https://www.instagram.com/andresprado_2525/'>
-                <img src="/img/instagram2.png" alt='instagram' />
+                <img src="/img/instagramLogo.svg" alt='instagram' />
             </a>
         </div>
-        <img src='/img/Logo.png' alt='org' />
+        <img src='/img/Logo.webp' alt='org' />
         <strong>Desarrollado por Andres Prado 2023</strong>
     </footer>
 }
